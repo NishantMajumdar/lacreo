253b2a70a239f3e37c01fa515c985540608451e7
Nishant Majumdar <mnishant1@outlook.com>
2023-10-07 09:19:53 +0530

    Update auth.js

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const jwtSecret = 'SUPERSECRETE20220';
 
 const saltRounds = 10;
-const uri = "mongodb+srv://user:123@lacreodb.i4zhcd5.mongodb.net/?retryWrites=true&w=majority";
+const url = "mongodb+srv://user:123@lacreodb.i4zhcd5.mongodb.net/?retryWrites=true&w=majority";
 const dbName = 'login';
 
 const client = new MongoClient(url, {
